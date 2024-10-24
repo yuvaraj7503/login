25622598f078e79ea44a3c4256e0e91d68c7ebd6
Yuvaraj <149352153+yuvaraj7503@users.noreply.github.com>
2024-10-24 21:13:23 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+
+const displayInfo = () => {
+    
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+
+    
+    const output = document.getElementById('output');
+    output.textContent = {name};
+};
